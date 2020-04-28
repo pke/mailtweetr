0949c025a98994bc45968b7a85d027741168a6a4
Philipp Kursawe <phil.kursawe@gmail.com>
2020-04-28 09:53:04 +0200

    chore: Refactor

--- bin/webhook.js
+++ bin/webhook.js
@@ -1,30 +1,29 @@
-//#! node
-
 require("dotenv").config()
 const https = require("https")
+
+console.log(process.env)
 
 const request = https.request({
   method: "POST",
   hostname: "api.trello.com",
   path: `/1/tokens/${process.env.TRELLO_API_TOKEN}/webhooks/`,
-  headers:  {
+  headers: {
     "Content-Type": "application/json",
   }
 }, function(response) {
   console.log(`statusCode: ${response.statusCode}`)
-
-  res.on('data', d => {
-    process.stdout.write(d)
-  })
+  const out = process.stdout.write.bind(process.stdout)
+  response.on("data", out)
+  response.on("end", out)
 })
 request.on("error", function(error) {
-  console.log(error)
+  console.error(error)
   process.exit(1)
 })
 request.write(JSON.stringify({
   key: process.env.TRELLO_API_KEY,
   idModel: process.env.TRELLO_INCOMING_LIST_ID,
-  callbackURL: process.env.NOW_URL + "/api/trello",
+  callbackURL: process.env.TRELLO_CALLBACK_URL,
   description: "tweetmailr hook for incoming list changes"
 }))
 request.end()
