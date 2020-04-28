8fad30e69d32674cfe293d4fb4bc67547a741e34
Philipp Kursawe <phil.kursawe@gmail.com>
2020-04-28 09:44:55 +0200

    chore: Remove dead code

--- src/twitter/tweetThread.js
+++ src/twitter/tweetThread.js
@@ -6,7 +6,7 @@ const client = new Twitter({
   consumer_secret: process.env["TWITTER_CONSUMER_SECRET"],
   access_token_key: process.env["TWITTER_ACCESS_TOKEN"],
   access_token_secret: process.env["TWITTER_ACCESS_TOKEN_SECRET"],
-})  
+})
 
 async function tweetThread(thread) {
   thread = Array.isArray(thread) ? thread : [thread]
@@ -19,9 +19,9 @@ async function tweetThread(thread) {
   try {
     for (const status of thread) {
       const tweet = await client.post("statuses/update", {
-          status,
-          in_reply_to_status_id: result.lastTweetID,
-          auto_populate_reply_metadata: true
+        status,
+        in_reply_to_status_id: result.lastTweetID,
+        auto_populate_reply_metadata: true
       })
       result.tweets.push(tweet)
       ++result.threadIndex
@@ -39,13 +39,3 @@ async function tweetThread(thread) {
 }
 
 module.exports = tweetThread
-
-if (require.main === module) {
-  //tweetThread(["TweetMailr test"]).catch(console.error)
-  /*client.get("account/verify_credentials")
-  .then(results => {
-    console.log("results", results)
-  })
-  .catch(console.error)*/
-} else {
-}
